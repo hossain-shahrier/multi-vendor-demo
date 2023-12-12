c9646139d7429046ab9c759464e13032696ce747
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-12-13 02:01:19 +0600

    All setup done

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+const ErrorHandler = require('../utils/ErrorHandler.js')
+const catchAsyncErrors = require("./catch-async-error.js");
+const jwt = require("jsonwebtoken");
+
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+});
